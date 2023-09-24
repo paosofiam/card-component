f16c41ef85655c2a7a078694221acb4f88265a04
Paola Sofía Mendoza <117321550+paosofiam@users.noreply.github.com>
2023-09-24 07:01:01 +0000

    Update 4 files

---
+++ assets/data.js
@@ -0,0 +1,22 @@
+var data = {
+  0: {
+    category: "Reaction",
+    score: 80,
+    icon: "./assets/images/icon-reaction.svg",
+  },
+  1: {
+    category: "Memory",
+    score: 92,
+    icon: "./assets/images/icon-memory.svg",
+  },
+  2: {
+    category: "Verbal",
+    score: 61,
+    icon: "./assets/images/icon-verbal.svg",
+  },
+  3: {
+    category: "Visual",
+    score: 72,
+    icon: "./assets/images/icon-visual.svg",
+  },
+};
